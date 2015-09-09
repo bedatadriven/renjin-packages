c401e61101027e863f2555eb2e85a2068122400b
Alex Bertram <alex@bedatadriven.com>
2015-09-09 16:08:06 +0200

    Added 'parallel' as core package

--- model/src/main/java/org/renjin/ci/model/CorePackages.java
+++ model/src/main/java/org/renjin/ci/model/CorePackages.java
@@ -11,7 +11,7 @@ public class CorePackages {
     Sets.newHashSet(
       "base", "stats", "stats4", "graphics", "grDevices",
       "utils", "methods", "datasets", "splines", "grid",
-      "splines");
+      "splines", "parallel");
 
   public static final List<String> DEFAULT_PACKAGES =
     Lists.newArrayList("stats", "graphics", "grDevices",

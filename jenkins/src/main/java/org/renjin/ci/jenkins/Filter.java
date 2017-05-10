f2f90b74a6528c3347b54d8675114a5ef5a595cb
Alex Bertram <alex@bedatadriven.com>
2017-05-10 21:14:27 +0200

    Fixed include/exclude filters in benchmark step

--- jenkins/src/main/java/org/renjin/ci/jenkins/Filter.java
+++ jenkins/src/main/java/org/renjin/ci/jenkins/Filter.java
@@ -1,19 +1,37 @@
 package org.renjin.ci.jenkins;
 
 
-import com.google.api.client.repackaged.com.google.common.base.Strings;
 import com.google.common.base.Predicate;
+import com.google.common.base.Strings;
 import org.renjin.ci.jenkins.benchmark.Benchmark;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 public class Filter implements Predicate<Benchmark> {
+
+  private static final Logger LOGGER = Logger.getLogger(Filter.class.getName());
+
   private String includes;
-  private String excludes[];
+  private List<String> excludes;
   private boolean noDependencies;
 
   public Filter(String includes, String excludes, boolean noDependencies) {
     this.includes = includes;
-    this.excludes = Strings.nullToEmpty(excludes).split("\\s*,\\s*");
+    this.excludes = parseExcludes(excludes);
     this.noDependencies = noDependencies;
+  }
+
+  private static List<String> parseExcludes(String excludeList) {
+    String[] patterns = Strings.nullToEmpty(excludeList).split("\\s*,\\s*");
+    List<String> excludes = new ArrayList<String>();
+    for (String pattern : patterns) {
+      if(!Strings.isNullOrEmpty(pattern)) {
+        excludes.add(pattern);
+      }
+    }
+    return excludes;
   }
 
   public boolean apply(Benchmark benchmark) {
@@ -21,6 +39,7 @@ public class Filter implements Predicate<Benchmark> {
       return false;
     }
     if(noDependencies && benchmark.hasDependencies()) {
+      LOGGER.info("Rejecting " + benchmark.getName() + " because it has dependencies...");
       return false;
     }
     return true;
@@ -28,10 +47,12 @@ public class Filter implements Predicate<Benchmark> {
 
   boolean apply(String name) {
     if(!Strings.isNullOrEmpty(includes) && !name.contains(includes)) {
+      LOGGER.info("Rejecting " + name + " because it does not match includes");
       return false;
     }
     for (String exclude : excludes) {
       if(name.contains(exclude)) {
+        LOGGER.info("Rejecting " + name + " because it matches exclude '" + exclude + "'");
         return false;
       }
     }

75b4bb5edeea0688ad18ad3efbd96980b9e60ef1
Alex Bertram <alex@bedatadriven.com>
2021-07-31 09:09:45 +0200

    Add directory listing to maven repository

--- webapp/src/main/java/org/renjin/ci/repo/NexusArchiveResource.java
+++ webapp/src/main/java/org/renjin/ci/repo/NexusArchiveResource.java
@@ -3,16 +3,18 @@ package org.renjin.ci.repo;
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
-import com.google.appengine.tools.cloudstorage.GcsFileMetadata;
-import com.google.appengine.tools.cloudstorage.GcsFilename;
-import com.google.appengine.tools.cloudstorage.GcsService;
-import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
+import com.google.appengine.tools.cloudstorage.*;
+import com.google.common.escape.Escaper;
+import com.google.common.html.HtmlEscapers;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import java.io.IOException;
+import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,8 +30,16 @@ public class NexusArchiveResource {
   public static final BlobstoreService BLOBSTORE_SERVICE = BlobstoreServiceFactory.getBlobstoreService();
 
   @GET
+  public Response getRootListing(@Context UriInfo uri) throws IOException {
+    if(!uri.getPath().endsWith("/")) {
+      return redirectToDirectory(uri);
+    }
+    return getDirectoryListing("");
+  }
+
+  @GET
   @Path("{path:.+}")
-  public Response getArtifact(@PathParam("path") String path) {
+  public Response getArtifact(@Context UriInfo uri, @PathParam("path") String path) throws IOException {
 
     LOGGER.log(Level.INFO, "Looking for artifact " + path);
 
@@ -39,12 +49,15 @@ public class NexusArchiveResource {
 
     GcsFileMetadata metadata;
     try {
-      metadata = GCS_SERVICE.getMetadata(new GcsFilename("renjin-nexus-archive", path));
+      metadata = GCS_SERVICE.getMetadata(   new GcsFilename("renjin-nexus-archive", path));
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
 
     if(metadata == null) {
+      if(isDirectory(path)) {
+        return redirectToDirectory(uri);
+      }
       return notFound();
     }
 
@@ -60,6 +73,19 @@ public class NexusArchiveResource {
       .build();
   }
 
+  private Response redirectToDirectory(UriInfo uri) {
+    URI directoryUri = uri.getAbsolutePathBuilder().replacePath(uri.getPath() + "/").build();
+    return Response.status(Response.Status.MOVED_PERMANENTLY).location(directoryUri).build();
+  }
+
+  private boolean isDirectory(String path) throws IOException {
+    ListResult result = GCS_SERVICE.list("renjin-nexus-archive", new ListOptions.Builder()
+            .setPrefix("path")
+            .setRecursive(false)
+            .build());
+    return result.hasNext();
+  }
+
   private Response notFound() {
     return Response.status(404)
       .type("text/html")
@@ -67,10 +93,38 @@ public class NexusArchiveResource {
       .build();
   }
 
-  private Response getDirectoryListing(String path) {
-    return Response.status(404)
+  private Response getDirectoryListing(String path) throws IOException {
+    ListResult result = GCS_SERVICE.list("renjin-nexus-archive", new ListOptions.Builder()
+            .setPrefix("path")
+            .setRecursive(false)
+            .build());
+
+    if(!result.hasNext()) {
+      return Response.status(Response.Status.NOT_FOUND).entity("<h1>Not found</h1>").build();
+    }
+
+    Escaper escaper = HtmlEscapers.htmlEscaper();
+    StringBuilder html = new StringBuilder();
+    html.append("<h1>").append("Index of ").append(escaper.escape(path)).append("</h1>\n");
+    html.append("<table><tr><th>Name</th><th>Last modified</th><th>Size</th></tr>\n");
+    result.forEachRemaining(item -> {
+      String escapedName = escaper.escape(item.getName());
+      if(item.isDirectory()) {
+        escapedName += "/";
+      }
+      html.append("<tr>");
+      html.append("<td><a href=\"").append(escapedName).append("\">").append(escapedName).append("</a></td>\n");
+      html.append("<td>").append(item.getLastModified().toString()).append("</td>\n");
+      if(!item.isDirectory()) {
+        html.append("<td>").append(item.getLength()).append("</td>");
+      }
+      html.append("</tr>");
+    });
+    html.append("</table>");
+
+    return Response.status(200)
       .type("text/html")
-      .entity("<h1>Directory listing disabled.</h1>")
+      .entity(html.toString())
       .build();
   }
 
